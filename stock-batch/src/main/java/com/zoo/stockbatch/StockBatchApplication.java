4ec65448b87829255c874fec71a0c88d0b31dfb8
suagramm <suagramm@gmail.com>
2021-03-19 23:40:25 +0900

    solved cross-module issue but remain pk issue

--- stock-batch/src/main/java/com/zoo/stockbatch/StockBatchApplication.java
+++ stock-batch/src/main/java/com/zoo/stockbatch/StockBatchApplication.java
@@ -1,13 +1,19 @@
 package com.zoo.stockbatch;
 
+import com.zoo.stockcommon.domain.Stock;
+import com.zoo.stockcommon.repository.StockRepository;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableBatchProcessing
+
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.zoo.stockcommon"})
+@EntityScan(basePackageClasses = {Stock.class})
+@EnableJpaRepositories(basePackageClasses = {StockRepository.class})
 public class StockBatchApplication {
 
     public static void main(String[] args) {
